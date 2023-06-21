e99187c253cbbfced5c38f138e64ecfb200322ac
Junior Avelino <junior.a98@hotmail.com>
2023-06-21 19:47:36 -0300

    refact: added util class to calculated divisibles

--- src/main/java/com/bysix/bysixtest/service/PrintNumberService.java
+++ src/main/java/com/bysix/bysixtest/service/PrintNumberService.java
@@ -1,7 +1,16 @@
 package com.bysix.bysixtest.service;
 
+import com.bysix.bysixtest.Enum.VNEnum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.bysix.bysixtest.util.UtilMath.divisibleByFive;
+import static com.bysix.bysixtest.util.UtilMath.divisibleByThree;
+
 public class PrintNumberService {
     private static final int NUMBER = 100;
+    private Map<VNEnum, String> messages;
 
     public void print(){
         for(int i = 1; i <= NUMBER; i++){
@@ -9,26 +18,29 @@ public class PrintNumberService {
         }
     }
 
-    public String correctedValue(int number) {
+    public PrintNumberService(){
+        setMessages();
+    }
 
+    private void setMessages() {
+        messages = new HashMap<>();
+        messages.put(VNEnum.VISUAL, "Visual");
+        messages.put(VNEnum.NUTS, "Nuts");
+        messages.put(VNEnum.VISUAL_NUTS, "Visual Nuts");
+    }
+
+    public String correctedValue(int number) {
         boolean isVisual = divisibleByThree(number);
         boolean isNuts = divisibleByFive(number);
+
         if (isVisual && isNuts) {
-            // teste
+            return messages.get(VNEnum.VISUAL_NUTS);
         } else if (isVisual) {
-            // visual return
+            return messages.get(VNEnum.VISUAL);
         } else if (isNuts) {
-            // nuts return
+            return messages.get(VNEnum.NUTS);
         } else {
-            // raw number return
+            return Integer.toString(number);
         }
-    }
-
-    private boolean divisibleByThree(int number) {
-        return (number % 3) == 0;
-    }
-
-    private boolean divisibleByFive(int number) {
-        return (number % 5) == 0;
     }
 }
---
+++ src/main/java/com/bysix/bysixtest/util/UtilMath.java
@@ -0,0 +1,12 @@
+package com.bysix.bysixtest.util;
+
+public class UtilMath {
+
+    public static boolean divisibleByThree(int number) {
+        return (number % 3) == 0;
+    }
+
+    public static boolean divisibleByFive(int number) {
+        return (number % 5) == 0;
+    }
+}
