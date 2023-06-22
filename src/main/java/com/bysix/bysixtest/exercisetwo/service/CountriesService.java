9a26453bb2ea226883cd48e1ada458d0b5030d55
Junior Avelino <junior.a98@hotmail.com>
2023-06-22 14:35:32 -0300

    feat: refactoring functions to fit the design

--- src/main/java/com/bysix/bysixtest/exercisetwo/service/CountriesService.java
+++ src/main/java/com/bysix/bysixtest/exercisetwo/service/CountriesService.java
@@ -9,51 +9,49 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CountriesService {
-    private Stream<Country> countries;
-
-
-    public CountriesService() {
-        countries = Arrays.stream(new CountriesDTO().getAll());
-    }
-
     public long count() {
-        if (countries.anyMatch(country -> true)) {
+        Stream<Country> countries = Arrays.stream(new CountriesDTO().getAll());
+        //if (countries.anyMatch(country -> true)) {
             return countries.count();
-        }
-        return 0;
+        //}
+        //return 0;
     }
 
-    public String findMostOfficialLanguagesSpeakGerman() {
-        if (countries.anyMatch(country -> true)) {
+    public String mostOfficialLanguagesSpeakGerman() {
+        Stream<Country> countries = Arrays.stream(new CountriesDTO().getAll());
+        //if (countries.anyMatch(country -> true)) {
             return countries.filter(c -> c.getLanguages().contains("de"))
                     .reduce((o1, o2) -> (o1.getLanguages().size() > o2.getLanguages().size()) ? o1 : o2)
                     .get()
                     .getCountry();
-        }
-        return null;
+        //}
+        //return null;
     }
 
     public long countLanguages() {
-        if (countries.anyMatch(country -> true)) {
+        Stream<Country> countries = Arrays.stream(new CountriesDTO().getAll());
+        //if (countries.anyMatch(country -> true)) {
             return countries.map(country -> country.getLanguages())
                     .flatMap(Collection::stream)
                     .distinct()
                     .count();
-        }
-        return 0;
+        //}
+        //return 0;
     }
 
-    public String findMostOfficialLanguages() {
-        if (countries.anyMatch(country -> true)) {
+    public String mostOfficialLanguages() {
+        Stream<Country> countries = Arrays.stream(new CountriesDTO().getAll());
+        //if (countries.anyMatch(country -> true)) {
             return countries.reduce((o1, o2) -> (o1.getLanguages().size() > o2.getLanguages().size()) ? o1 : o2)
                     .get()
                     .getCountry();
-        }
-        return null;
+        //}
+        //return null;
     }
 
-    public String findMostCommonLanguage() {
-        if (countries.anyMatch(country -> true)) {
+    public String mostCommonLanguage() {
+        Stream<Country> countries = Arrays.stream(new CountriesDTO().getAll());
+        //if (countries.anyMatch(country -> true)) {
             return countries.map(country -> country.getLanguages())
                     .flatMap(Collection::stream)
                     .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
@@ -61,7 +59,15 @@ public class CountriesService {
                     .max(Comparator.comparing(Map.Entry::getValue))
                     .map(Map.Entry::getKey)
                     .get();
-        }
-        return null;
+        //}
+        //return null;
+    }
+
+    public void execute(){
+        System.out.println(count());
+        System.out.println(mostOfficialLanguagesSpeakGerman());
+        System.out.println(countLanguages());
+        System.out.println(mostOfficialLanguages());
+        System.out.println(mostCommonLanguage());
     }
 }
